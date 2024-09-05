7f762568f9c3905b99fee45d05c340c72342da92
Alka Sah <alkasah485@gmail.com>
2024-09-05 00:40:14 -0700

    modification saved

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 //---------------------dependency
 import Navbar from './components/Navbar'
+import Intro from './components/Intro'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Intro/>
       <Routes>
         {/* <Route path= "/" element={}/> */}
       </Routes>
---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <section id='intro'>
+        <div className="introContent">
+            <span className="hello">Hello,</span>
+            <span className="introText">I'm <span className="introName">Alka <br/> Web Developer</span></span>
+        </div>
+    
+    </section>
+  )
+}
+
+export default Intro
